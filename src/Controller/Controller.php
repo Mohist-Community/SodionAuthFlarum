1cd413c261b5f3ec980a282fea4a5298d30cd463
hs-err <1102280066@qq.com>
2020-10-10 01:34:45 +0800

    use sdk

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,34 +2,19 @@
 
 namespace Mohist\SodionAuthFlarum\Controller;
 
-use Flarum\Api\JsonApiResponse;
 use Laminas\Diactoros\Response\JsonResponse;
-use Mohist\SodionAuthFlarum\Response\Response;
+use Mohist\SodionAuthFlarum\Provider\UserProvider;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Tobscure\JsonApi\Document;
 
 class Controller implements RequestHandlerInterface
 {
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $response=$this->hand((Object)$request->getParsedBody());
+        $response = (new \Mohist\SodionAuth\Controller\Controller(new UserProvider()))
+            ->handle((object)$request->getParsedBody());
         return new JsonResponse($response->data);
-    }
-    protected function hand($request){
-        switch ($request->action){
-            case 'register':
-                return (new RegisterController())->hand($request);
-            case 'login':
-                return (new LoginController())->hand($request);
-            case 'loginEmail':
-                return (new LoginEmailController())->hand($request);
-            case 'join':
-                return (new JoinController())->hand($request);
-            default:
-                return Response::unknown('flarum_error','ah?'.$request->action);
-        }
     }
 }
--- src/Provider/UserProvider.php
+++ src/Provider/UserProvider.php
@@ -6,68 +6,89 @@ namespace Mohist\SodionAuthFlarum\Provider;
 
 use Flarum\User\User;
 use Flarum\User\UserRepository;
-use Illuminate\Support\Facades\DB;
+use Mohist\SodionAuth\Result\EmailExistResult;
+use Mohist\SodionAuth\Result\NameIncorrectResult;
+use Mohist\SodionAuth\Result\NoSuchUserResult;
+use Mohist\SodionAuth\Result\PasswordIncorrectResult;
+use Mohist\SodionAuth\Result\SuccessResult;
+use Mohist\SodionAuth\Result\UnknownResult;
+use Mohist\SodionAuth\Result\UserExistResult;
 
-class UserProvider
+class UserProvider extends \Mohist\SodionAuth\Provider\UserProvider
 {
-    public function canJoin($username){
-        $user=User::where('username', $username);
-        if(!$user){
-            $user=User::where('username', 'like', '%'.$this->escapeLikeString($username).'%')
-                ->first();
-            if($user){
-                return null;
-            }else{
-                return false;
-            }
-        }
-        return true;
-    }
-    public function login($username,$password){
+    public function nameVerify($username)
+    {
         /** @var User $user */
-        $user=User::where('username', $username);
-        if(!$user){
-            $user=User::where('username', 'like', '%'.$this->escapeLikeString($username).'%')
+        $user = User::where('username', $username)->first();
+        if (!$user) {
+            $user = User::where('username', 'like', '%' . $this->escapeLikeString($username) . '%')
                 ->first();
-            if($user){
-                return $user->username;
-            }else{
-                return false;
+            if ($user) {
+                return new NameIncorrectResult([
+                    'correct' => $user->username
+                ]);
+            } else {
+                return new NoSuchUserResult();
             }
         }
-        if($user->checkPassword($password)){
-            return true;
-        }
-        return false;
+        return new SuccessResult();
     }
-    public function loginEmail($email,$password){
-        $user=(new UserRepository())->findByEmail($email);
-        if(!$user){
-            return null;
-        }
-        if($user->checkPassword($password)){
-            return $user->username;
-        }else{
-            return false;
-        }
-    }
-    public function register($username,$email,$password){
-        $user=User::where('username',$username)->first();
-        if($user){
-            return 0;
-        }
-        $user=User::where('email',$email)->first();
-        if($user){
-            return 1;
-        }
-        $user=User::register($username,$email,$password);
-        if(!$user){
-            return false;
-        }
-        return true;
-    }
+
     private function escapeLikeString($string)
     {
         return str_replace(['\\', '%', '_'], ['\\\\', '\%', '\_'], $string);
+    }
+
+    public function login($username, $password)
+    {
+        /** @var User $user */
+        $user = User::where('username', $username)->first();
+        if (!$user) {
+            $user = User::where('username', 'like', '%' . $this->escapeLikeString($username) . '%')
+                ->first();
+            if ($user) {
+                return new NameIncorrectResult([
+                    'correct' => $user->username
+                ]);
+            } else {
+                return new NoSuchUserResult();
+            }
+        }
+        if ($user->checkPassword($password)) {
+            return new SuccessResult();
+        }
+        return new PasswordIncorrectResult();
+    }
+
+    public function loginEmail($email, $password)
+    {
+        $user = (new UserRepository())->findByEmail($email);
+        if (!$user) {
+            return new NoSuchUserResult();
+        }
+        if ($user->checkPassword($password)) {
+            return new SuccessResult([
+                'correct' => $user->username
+            ]);
+        } else {
+            return new PasswordIncorrectResult();
+        }
+    }
+
+    public function register($username, $email, $password)
+    {
+        $user = User::where('username', $username)->first();
+        if ($user) {
+            return new UserExistResult();
+        }
+        $user = User::where('email', $email)->first();
+        if ($user) {
+            return new EmailExistResult();
+        }
+        $user = User::register($username, $email, $password);
+        if (!$user) {
+            return new UnknownResult();
+        }
+        return new SuccessResult();
     }
 }
